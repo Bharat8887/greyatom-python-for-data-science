7247445a6a8529059b2e6802535ae692bcb517ff
Bharat8887 <Bharat8887@users.noreply.github.com>
2020-04-28 11:52:02 +0530

    Code file created.

---
+++ Spy-Games/code.py
@@ -0,0 +1,116 @@
+# --------------
+#***************************STEP 1
+#Code starts here
+file_path
+#Function to read file
+def read_file(path):
+    file=open(path,"r")
+    sentence=file.readline()
+    file.close()
+    return sentence
+   
+sample_message = read_file(file_path) 
+print(sample_message)
+  
+file_path_1
+file_path_2
+message_1=read_file(file_path_1)
+message_2=read_file(file_path_2) 
+print(message_1)
+print(message_2)
+    
+#*************************************STEP 2
+
+#Function to fuse message
+def fuse_msg(message_a,message_b):
+    
+    #Integer division of two numbers
+    quotient=int(message_b)//int(message_a)
+    #Returning the quotient in string format
+    return str(quotient)
+#Calling the function to read file  
+secret_msg_1 = fuse_msg(message_1,message_2)
+print(secret_msg_1)
+
+
+#********************************STEP 3
+
+file_path_3
+message_3=read_file(file_path_3)
+print(message_3)
+#Function to substitute the message
+def substitute_msg(message_c):
+    sub = ''
+    if message_c == 'Red':
+        sub = 'Army General'
+    elif message_c == 'Green':
+        sub = 'Data Scientist'
+    else:
+        sub = 'Marine Biologist'
+    return sub
+
+secret_msg_2 = substitute_msg(message_3)
+print(secret_msg_2)
+   
+
+#****************************************************STEP 4
+
+
+file_path_4
+file_path_5
+message_4=read_file(file_path_4)
+message_5=read_file(file_path_5)
+print(message_4)
+print(message_5)
+#Function to compare message
+def compare_msg(message_d,message_e):
+    a_list=message_d.split()
+    b_list=message_e.split()
+    c_list=[]
+    for item in a_list:
+        if item not in b_list:
+            c_list.append(item)
+    final_msg = " ".join(c_list)
+    return final_msg
+    
+secret_msg_3 = compare_msg(message_4, message_5)
+print(secret_msg_3)
+
+#********************************************************STEP 5
+
+file_path_6
+message_6=read_file(file_path_6)
+print(message_6)  
+    
+def extract_msg(message_f):
+    a_list=message_f.split()
+    even_words=lambda x:(len(x)%2==0)
+    b_list=list(filter(even_words,a_list))
+    final_msg=" ".join(b_list)
+    return final_msg
+
+secret_msg_4 = extract_msg(message_6)
+print(secret_msg_4)
+
+
+#************************************************************* STEP 6
+
+
+message_parts=[secret_msg_3, secret_msg_1, secret_msg_4, secret_msg_2]
+
+
+final_path= user_data_dir + '/secret_message.txt'
+
+secret_msg = " ".join(message_parts)
+
+def write_file(secret_msg, path):
+    with open(path, 'a+') as file:
+        file.write(secret_msg)
+        file.close()
+
+write_file(secret_msg, final_path)
+print(secret_msg)
+
+ 
+
+
